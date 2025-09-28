253437a67e6f4ef5a011ddff3d71b4e0fd41ca31
thanhhoa1309 <hoahttse161039@fpt.edu.vn>
2025-09-29 01:29:41 +0700

    test2

--- EVDealerSales.Models/DTOs/QuoteDTOs/QuoteDto.cs
+++ EVDealerSales.Models/DTOs/QuoteDTOs/QuoteDto.cs
@@ -6,7 +6,8 @@
         virtual public string CustomerName { get; set; }
         virtual public string CustomerEmail { get; set; }
         virtual public string VehicleModel { get; set; }
-        virtual public decimal Price { get; set; }
+        virtual public decimal Price { get; set; } = 0;
+        virtual public DateTime QuoteDate { get; set; } = DateTime.UtcNow;
 
     }
 }
