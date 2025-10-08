70ae12d55431f457220c59b29c2d5532c5493c00
Dessmin <quanglong281381@gmail.com>
2025-10-08 16:33:48 +0700

    test

--- EVDealerSales.BO/DTOs/AuthDTOs/LoginRequestDto.cs
+++ EVDealerSales.BO/DTOs/AuthDTOs/LoginRequestDto.cs
@@ -2,6 +2,7 @@
 {
     public class LoginRequestDto
     {
+        // Using 'required' to ensure these properties must be set during object initialization
         public required string Email { get; set; }
         public required string Password { get; set; }
     }
